13f3103fe3aa29c63d0ed94a9196f674a782c780
Alexander Lønbæk Hahn Christensen <alexanderlhc@disroot.org>
2021-02-10 11:08:06 +0100

    Missed the _secondsSinceMidnight part, partly added

--- 20210208-L03-TimeStruct/Program.cs
+++ 20210208-L03-TimeStruct/Program.cs
@@ -9,9 +9,10 @@ namespace _20210208_L03_TimeStruct
         public Time(string timeIn)
         {
             try {
-              Hours   = Int32.Parse(timeIn.Substring(0, 2));
-              Minutes = Int32.Parse(timeIn.Substring(3, 2));
-              Seconds = Int32.Parse(timeIn.Substring(6, 2));
+              int hours   = Int32.Parse(timeIn.Substring(0, 2));
+              int minutes = Int32.Parse(timeIn.Substring(3, 2));
+              int seconds = Int32.Parse(timeIn.Substring(6, 2));
+              _secondsFromMidnight = ((hours * 60 * 60) + (minutes * 60) + seconds);
             }
             catch (Exception e)
             {
@@ -21,18 +22,29 @@ namespace _20210208_L03_TimeStruct
 
         public Time(int hours, int minutes, int seconds)
         {
-            Hours   = hours;
-            Minutes = minutes;
-            Seconds = seconds;
+          _secondsFromMidnight = ((hours * 60 * 60) + (minutes * 60) + seconds);
         }
 
-        public int Hours { get; set; }
-        public int Minutes { get; set; }
-        public int Seconds { get; set; }
+        private int _secondsFromMidnight;
+        public int Hours
+        {
+          get { return _secondsFromMidnight / (60 * 60); }
+          set
+          {
+            int hours = _secondsFromMidnight / 60 * 60;
+            _secondsFromMidnight = _secondsFromMidnight - hours + (value * 60 * 60);
+          }
+        }
+        public int Minutes {
+          get { return (_secondsFromMidnight % (60 * 60)) / 60; }
+        }
+        public int Seconds {
+          get { return _secondsFromMidnight % 60; }
+        }
 
         public override string ToString()
         {
-          return String.Format("{0}:{1}:{2}", Hours.ToString("00"), Minutes.ToString("00"), Seconds.ToString("00"));
+          return String.Format("{0}:{1}:{2} - {3}", Hours.ToString("00"), Minutes.ToString("00"), Seconds.ToString("00"), _secondsFromMidnight);
         }
     }
 
@@ -41,7 +53,7 @@ namespace _20210208_L03_TimeStruct
         static void Main(string[] args)
         {
           List<Time> times = new List<Time>() {
-            new Time("00:88:32"), new Time(12,04,39), new Time(1,2,3)
+            new Time("00:55:32"), new Time(12,04,39), new Time(1,2,3)
           };
 
           foreach (Time time in times)
